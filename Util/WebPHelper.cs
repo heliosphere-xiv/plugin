94da4b8dc7fbc5b615d34c801c3086c86c7403c2
Anna Clemens <git@annaclemens.io>
2023-01-30 19:25:38 -0500

    fix: use rgba for webp

--- Util/WebPHelper.cs
+++ Util/WebPHelper.cs
@@ -1,39 +1,17 @@
-using System.Runtime.InteropServices;
 using Dalamud.Interface;
+using Dalamud.Memory;
 using ImGuiScene;
-using WebP.Net.Natives;
-using WebP.Net.Natives.Enums;
-using WebP.Net.Natives.Structs;
+using WebPDotNet;
 
 namespace Heliosphere.Util;
 
 internal static class WebPHelper {
     internal static async Task<TextureWrap> LoadAsync(UiBuilder builder, byte[] imageBytes) {
-        byte[] outputBuffer;
-        WebPBitstreamFeatures features;
         const int bytesPerPixel = 4;
 
-        unsafe {
-            fixed (byte* bytes = imageBytes) {
-                features = new WebPBitstreamFeatures();
-                var vp8StatusCode = Native.WebPGetFeatures((nint) bytes, imageBytes.Length, ref features);
-                if (vp8StatusCode != Vp8StatusCode.Ok) {
-                    throw new ExternalException(vp8StatusCode.ToString());
-                }
+        using var image = WebP.WebPDecodeRGBA(imageBytes);
+        var outputBuffer = MemoryHelper.ReadRaw(image.NativePtr, image.Height * image.Width * bytesPerPixel);
 
-                var stride = features.Width * bytesPerPixel;
-                var size = features.Height * stride;
-                outputBuffer = new byte[size];
-                fixed (byte* output = outputBuffer) {
-                    var result = Native.WebPDecodeBgraInto((nint) bytes, imageBytes.Length, (nint) output, size, stride);
-
-                    if (result == 0) {
-                        throw new ExternalException();
-                    }
-                }
-            }
-        }
-
-        return await builder.LoadImageRawAsync(outputBuffer, features.Width, features.Height, bytesPerPixel);
+        return await builder.LoadImageRawAsync(outputBuffer, image.Width, image.Height, bytesPerPixel);
     }
 }
