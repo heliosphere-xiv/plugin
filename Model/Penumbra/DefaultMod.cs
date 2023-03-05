9c06627dcfca5f4501115008712a54e0251b4eaf
Anna Clemens <git@annaclemens.io>
2023-03-05 13:59:40 -0500

    fix: split large groups into parts

--- Model/Penumbra/DefaultMod.cs
+++ Model/Penumbra/DefaultMod.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Heliosphere.Model.Penumbra;
@@ -9,4 +10,6 @@ internal class DefaultMod {
     public Dictionary<string, string> Files { get; set; } = new();
     public Dictionary<string, string> FileSwaps { get; set; } = new();
     public List<JToken> Manipulations { get; set; } = new();
+    [JsonIgnore]
+    public bool IsDefault { get; set; }
 }
