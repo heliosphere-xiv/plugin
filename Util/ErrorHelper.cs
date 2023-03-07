7b92f48c65203d76d1af00ae382cc7d3ecb85153
Anna Clemens <git@annaclemens.io>
2023-03-07 04:24:13 -0500

    chore: include sentry event id in logs

--- Util/ErrorHelper.cs
+++ Util/ErrorHelper.cs
@@ -5,9 +5,10 @@ namespace Heliosphere.Util;
 
 internal static class ErrorHelper {
     internal static void Handle(Exception ex, string message) {
-        SentrySdk.CaptureException(ex, scope => scope.Contexts["ErrorHelper"] = new {
+        var errorId = SentrySdk.CaptureException(ex, scope => scope.Contexts["ErrorHelper"] = new {
             Message = message,
         });
-        PluginLog.LogError(ex, message);
+
+        PluginLog.LogError(ex, $"[{errorId}] {message}");
     }
 }
