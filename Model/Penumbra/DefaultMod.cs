2fe7a4150956640fb54d9133d89e8d89e2aa56de
Anna <git@anna.lgbt>
2023-03-07 23:31:54 -0500

    fix: include descriptions for options

--- Model/Penumbra/DefaultMod.cs
+++ Model/Penumbra/DefaultMod.cs
@@ -6,10 +6,12 @@ namespace Heliosphere.Model.Penumbra;
 [Serializable]
 internal class DefaultMod {
     public string Name { get; set; } = string.Empty;
-    public int Priority { get; set; } = 0;
+    public string? Description { get; set; }
+    public int Priority { get; set; }
     public Dictionary<string, string> Files { get; set; } = new();
     public Dictionary<string, string> FileSwaps { get; set; } = new();
     public List<JToken> Manipulations { get; set; } = new();
+
     [JsonIgnore]
     public bool IsDefault { get; set; }
 }
