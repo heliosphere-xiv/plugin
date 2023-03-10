56bd3c046b764ca6422613c36ec504373ba4fb7f
Anna <git@anna.lgbt>
2023-03-09 20:39:26 -0500

    chore: add changelog support for the packaging script

--- make_repo.py
+++ make_repo.py
@@ -1,4 +1,5 @@
 from sys import argv
+from os import path
 from datetime import datetime
 import zipfile
 import json
@@ -6,34 +7,40 @@ import json
 
 def main():
     if len(argv) < 4:
-        print('make_repo.py [latest.zip] [download link] [output path]')
+        print('make_repo.py [latest.zip] [changelog.txt] [download link] [output path]')
         return
 
     args = {
         'zip_path': argv[1],
-        'download_link': argv[2],
-        'output_path': argv[3],
+        'changelog_path': argv[2],
+        'download_link': argv[3],
+        'output_path': argv[4],
     }
 
-    zip = zipfile.PyZipFile(args['zip_path'])
-    json_file = None
-    for f in zip.filelist:
+    zip_file = zipfile.PyZipFile(args['zip_path'])
+    json_file, json_data, changelog = None, None, None
+    for f in zip_file.filelist:
         if f.filename.endswith('.json'):
             json_file = f
-            with zip.open(json_file, 'r') as f:
-                json_text = f.read()
+            with zip_file.open(json_file, 'r') as inner:
+                json_text = inner.read()
             json_data = json.loads(json_text)
             if 'DalamudApiLevel' in json_data:
                 break
             json_file = None
-    if json_file is None:
+    if json_file is None or json_data is None:
         return
+    if path.exists(args['changelog_path']):
+        with open(args['changelog_path']) as f:
+            changelog = f.read().strip()
     json_data['DownloadLinkInstall'] = args['download_link']
     json_data['DownloadLinkTesting'] = args['download_link']
     json_data['DownloadLinkUpdate'] = args['download_link']
     json_data['IsHide'] = False
     json_data['IsTestingExclusive'] = False
     json_data['LastUpdate'] = int(datetime(*json_file.date_time).timestamp())
+    if changelog is not None and len(changelog) > 0:
+        json_data['Changelog'] = changelog
 
     with open(args['output_path'], 'w') as f:
         json.dump([json_data], f, indent=4, sort_keys=True)
---
+++ package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/zsh
+
+python3 make_repo.py \
+  bin/x64/Release/net7.0-windows/heliosphere-plugin/latest.zip \
+  ./changelog.txt \
+  https://repo.heliosphere.app/latest.zip \
+  repo.json
