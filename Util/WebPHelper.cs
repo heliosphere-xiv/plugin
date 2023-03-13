52792e071341967a342db0ef881771617085b9ce
Anna Clemens <git@annaclemens.io>
2023-03-12 20:06:27 -0400

    fix: don't pass null pointer to MemoryHelper

--- Util/WebPHelper.cs
+++ Util/WebPHelper.cs
@@ -1,4 +1,5 @@
 using Dalamud.Interface;
+using Dalamud.Logging;
 using Dalamud.Memory;
 using ImGuiScene;
 using WebPDotNet;
@@ -6,10 +7,15 @@ using WebPDotNet;
 namespace Heliosphere.Util;
 
 internal static class WebPHelper {
-    internal static async Task<TextureWrap> LoadAsync(UiBuilder builder, byte[] imageBytes) {
+    internal static async Task<TextureWrap?> LoadAsync(UiBuilder builder, byte[] imageBytes) {
         const int bytesPerPixel = 4;
 
         using var image = WebP.WebPDecodeRGBA(imageBytes);
+        if (image.NativePtr == nint.Zero) {
+            PluginLog.LogWarning("webp image had a null data pointer");
+            return null;
+        }
+
         var outputBuffer = MemoryHelper.ReadRaw(image.NativePtr, image.Height * image.Width * bytesPerPixel);
 
         return await builder.LoadImageRawAsync(outputBuffer, image.Width, image.Height, bytesPerPixel);
