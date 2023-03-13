f583372713d65e56acc8f80a53a1b113cbba760c
Anna Clemens <git@annaclemens.io>
2023-03-13 12:26:37 -0400

    feat: include hresult in exceptions to sentry

--- Util/ErrorHelper.cs
+++ Util/ErrorHelper.cs
@@ -7,6 +7,7 @@ internal static class ErrorHelper {
     internal static void Handle(Exception ex, string message) {
         var errorId = SentrySdk.CaptureException(ex, scope => scope.Contexts["ErrorHelper"] = new {
             Message = message,
+            HResult = (int?) (ex is IOException io ? io.HResult : null),
         });
 
         PluginLog.LogError(ex, $"[{errorId}] {message}");
