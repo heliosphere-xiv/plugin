611f02d00e8616bc76f9300b3a1ef820acde2171
Anna <git@anna.lgbt>
2023-03-12 20:06:02 -0400

    fix: wait for directory to exist

--- Util/PathHelper.cs
+++ Util/PathHelper.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Heliosphere.Util;
 
 internal static class PathHelper {
@@ -15,5 +17,25 @@ internal static class PathHelper {
 
     internal static string ChangeExtension(string path, string? ext) {
         return Path.ChangeExtension(GetBaseName(path), ext);
+    }
+
+    /// <summary>
+    /// Wait until <see cref="Directory.Exists"/> returns true for a path.
+    /// </summary>
+    /// <param name="path">Path to check</param>
+    /// <param name="timeout">Maximum time to wait before returning (default: 5 seconds)</param>
+    /// <param name="wait">Time to wait between each check (default: 100 milliseconds)</param>
+    /// <returns>Final result of <see cref="Directory.Exists"/></returns>
+    internal static async Task<bool> WaitToExist(string path, TimeSpan? timeout = null, TimeSpan? wait = null) {
+        var max = timeout ?? TimeSpan.FromSeconds(5);
+
+        var waiting = new Stopwatch();
+        waiting.Start();
+
+        while (!Directory.Exists(path) && waiting.Elapsed <= max) {
+            await Task.Delay(wait ?? TimeSpan.FromMilliseconds(100));
+        }
+
+        return Directory.Exists(path);
     }
 }
