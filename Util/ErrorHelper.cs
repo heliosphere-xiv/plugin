3f57854b39a4d08761ff9430b2cade87173bd266
Anna Clemens <git@annaclemens.io>
2023-03-31 23:47:46 -0400

    chore: report hresults as hex

--- Util/ErrorHelper.cs
+++ Util/ErrorHelper.cs
@@ -19,9 +19,12 @@ internal static class ErrorHelper {
     }
 
     internal static void Handle(Exception ex, string message) {
-        var errorId = SentrySdk.CaptureException(ex, scope => scope.Contexts["ErrorHelper"] = new {
+        var errorId = SentrySdk.CaptureException(ex, scope => scope.Contexts["Error Helper"] = new {
             Message = message,
-            HResults = ex.GetHResults().ToList(),
+            HResults = ex.GetHResults()
+                .Select(hr => (uint) hr)
+                .Select(hr => $"0x{hr:X8}")
+                .ToList(),
         });
 
         PluginLog.LogError(ex, $"[{errorId}] {message}");
