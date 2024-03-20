9052ba28e20dac08e07d7fcfa13908b2b1afa1af
Anna <git@anna.lgbt>
2024-03-20 14:29:53 -0400

    chore: bump version to 1.23.0

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 
 python3 make_repo.py \
-  bin/x64/Release/net7.0-windows/heliosphere-plugin/latest.zip \
+  bin/x64/Release/net8.0-windows/heliosphere-plugin/latest.zip \
   ./changelog.txt \
   https://repo.heliosphere.app/latest.zip \
   repo.json
