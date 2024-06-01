f6d9c869b2fa7694f696d5240355d504598c7344
Anna <git@anna.lgbt>
2024-06-01 05:00:54 -0400

    fix: don't serialise nulls for imc options

--- Model/Penumbra/ImcOption.cs
+++ Model/Penumbra/ImcOption.cs
@@ -1,9 +1,15 @@
+using Newtonsoft.Json;
+
 namespace Heliosphere.Model.Penumbra;
 
 [Serializable]
 internal class ImcOption {
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
+
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public bool? IsDisableSubMod { get; set; }
+
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public int? AttributeMask { get; set; }
 }
