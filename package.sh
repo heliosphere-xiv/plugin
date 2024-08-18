36e98278ec286aef51c038d8407809a6fe498be6
Anna <git@anna.lgbt>
2024-08-18 17:33:45 -0400

    chore: remove unused shell script

--- package.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/zsh
-
-python3 make_repo.py \
-  bin/x64/Release/net8.0-windows/heliosphere-plugin/latest.zip \
-  ./changelog.txt \
-  https://repo.heliosphere.app/latest.zip \
-  repo.json
