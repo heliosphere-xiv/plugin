da563e89b94e6e1dccaa6a04006968c09fc8603f
Anna <git@anna.lgbt>
2024-08-18 16:26:43 -0400

    chore(ci): add support for testing

---
+++ ci-tools.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import xml.etree.ElementTree as ET
+from os import environ
+from sys import argv, exit
+
+def get_version():
+    with open('heliosphere-plugin.csproj') as f:
+        tree = ET.parse(f)
+
+    root = tree.getroot()
+    root.find('')
+    items = list(filter(
+        lambda version: version is not None,
+        map(
+            lambda child: child.find('Version'),
+            root,
+        ),
+    ))
+
+    if len(items) == 0:
+        return None
+
+    return items[0].text
+
+def is_testing():
+    return environ.get('GITHUB_REF_NAME') == 'testing'
+
+def main():
+    subcommand = argv[1]
+    if subcommand == 'get-version':
+        version = get_version()
+        if version is None:
+            exit(1)
+        print(version)
+    elif subcommand == 'is-testing':
+        exit(0 if is_testing() else 1)
+    else:
+        print(f'unknown subcommand {subcommand}')
+        exit(1)
+
+if __name__ == '__main__':
+    main()
--- make_repo.py
+++ make_repo.py
@@ -1,24 +1,55 @@
-from sys import argv
 from os import path
 from datetime import datetime
+from requests import get
+import argparse
 import zipfile
 import json
 
 
+COPY_KEYS = [
+    'Author',
+    'Name',
+    'Punchline',
+    'Description',
+    'InternalName',
+    'RepoUrl',
+    'ApplicableVersion',
+    'IsHide',
+    'IsTestingExclusive',
+    'IconUrl',
+]
+
+MAYBE_TESTING_COPY_KEYS = [
+    'AssemblyVersion',
+    'DalamudApiLevel',
+]
+
 def main():
-    if len(argv) < 4:
-        print('make_repo.py [latest.zip] [changelog.txt] [download link] [output path]')
-        return
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--archive', help='the zip archive of the plugin', required=True)
+    parser.add_argument('--changelog', help='the changelog text file', required=True)
+    parser.add_argument('--changelog-test', help='the changelog text file for testing mode')
+    parser.add_argument('--download-main', help='the main download link', required=True)
+    parser.add_argument('--download-test', help='the testing download link (if any)')
+    parser.add_argument('--testing', help='enable/disable testing mode', action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument('output', help='the output path of the generated repo')
+
+    args = parser.parse_args()
 
     args = {
-        'zip_path': argv[1],
-        'changelog_path': argv[2],
-        'download_link': argv[3],
-        'output_path': argv[4],
+        'zip_path': args.archive,
+        'changelog_path': args.changelog,
+        'changelog_path_test': args.changelog_test,
+        'download_link_main': args.download_main,
+        'download_link_test': args.download_test,
+        'output_path': args.output,
+        'testing': args.testing
     }
 
+    repo = get('https://repo.heliosphere.app/').json()[0]
+
     zip_file = zipfile.PyZipFile(args['zip_path'])
-    json_file, json_data, changelog = None, None, None
+    json_file, json_data, changelog, changelog_test = None, None, None, None
     for f in zip_file.filelist:
         if f.filename.endswith('.json'):
             json_file = f
@@ -33,24 +64,37 @@ def main():
     if path.exists(args['changelog_path']):
         with open(args['changelog_path']) as f:
             changelog = f.read().strip()
-    json_data['DownloadLinkInstall'] = args['download_link']
-    json_data['DownloadLinkTesting'] = args['download_link']
-    json_data['DownloadLinkUpdate'] = args['download_link']
-    json_data['IsHide'] = False
-    json_data['IsTestingExclusive'] = False
+    if args['changelog_path_test'] is not None and path.exists(args['changelog_path_test']):
+        with open(args['changelog_path_test']) as f:
+            changelog_test = f.read().strip()
 
-    # json_data['IsTestingExclusive'] = True
-    # json_data['TestingAssemblyVersion'] = json_data['AssemblyVersion']
-    # json_data['TestingDalamudApiLevel'] = json_data['DalamudApiLevel']
-    # json_data['AssemblyVersion'] = '1.28.4.0'
-    # json_data['DalamudApiLevel'] = 9
+    for key in COPY_KEYS:
+        if key in json_data:
+            repo[key] = json_data[key]
 
-    json_data['LastUpdate'] = int(datetime(*json_file.date_time).timestamp())
+    for key in MAYBE_TESTING_COPY_KEYS:
+        if key in json_data:
+            prefix = 'Testing' if args['testing'] else ''
+            repo[prefix + key] = json_data[key]
+
+    repo['DownloadCount'] = 9001
+    repo['DownloadLinkInstall'] = args['download_link_main']
+    repo['DownloadLinkTesting'] = args['download_link_test'] if args['download_link_test'] is not None else args['download_link_main']
+    repo['DownloadLinkUpdate'] = args['download_link_main']
+    repo['IsHide'] = False
+    repo['IsTestingExclusive'] = False
+
+    if not args['testing']:
+        repo['LastUpdate'] = int(datetime(*json_file.date_time).timestamp())
+
     if changelog is not None and len(changelog) > 0:
-        json_data['Changelog'] = changelog
+        repo['Changelog'] = changelog
+
+    if changelog_test is not None and len(changelog_test) > 0:
+        repo['TestingChangelog'] = changelog_test
 
     with open(args['output_path'], 'w') as f:
-        json.dump([json_data], f, indent=4, sort_keys=True)
+        json.dump([repo], f, indent=4, sort_keys=True)
 
     print(f'wrote to {args["output_path"]}')
 
