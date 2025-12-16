111c86ab597cbe11ad3f120a025f6fcfc3d77bfb
Anna <git@anna.lgbt>
2025-12-16 12:35:16 -0500

    refactor: update RestartManager to new signatures

--- Util/RestartManager.cs
+++ Util/RestartManager.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using System.Runtime.InteropServices;
 using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.System.RestartManager;
@@ -24,17 +23,8 @@ internal static class RestartManager {
     }
 
     private static unsafe List<Process> GetLockingProcessesInner(string path) {
-        // var key = Guid.NewGuid().ToString().ToCharArray();
-        // var res = PInvoke.RmStartSession(out var handle, key.AsSpan());
-        var key = Guid.NewGuid().ToString();
-        var keyHandle = GCHandle.Alloc(key, GCHandleType.Pinned);
-        using var freeKeyHandle = new OnDispose(() => {
-            if (keyHandle.IsAllocated) {
-                keyHandle.Free();
-            }
-        });
-
-        var res = PInvoke.RmStartSession(out var handle, (char*) keyHandle.AddrOfPinnedObject());
+        var key = Guid.NewGuid().ToString().ToCharArray();
+        var res = PInvoke.RmStartSession(out var handle, key.AsSpan());
 
         if (res != WIN32_ERROR.NO_ERROR) {
             throw new Exception("Could not begin restart session. Unable to determine file locker.");
@@ -42,25 +32,17 @@ internal static class RestartManager {
 
         using var endSession = new OnDispose(() => PInvoke.RmEndSession(handle));
 
-
-        fixed (char* pathPtr = path) {
-            var paths = new PCWSTR [] { pathPtr };
-            res = PInvoke.RmRegisterResources(handle, paths, null, null);
-        }
-        // res = PInvoke.RmRegisterResources(handle, new Span<string>(ref path), [], []);
+        res = PInvoke.RmRegisterResources(handle, new Span<string>(ref path), [], []);
 
         if (res != WIN32_ERROR.NO_ERROR) {
             throw new Exception("Could not register resource.");
         }
 
         var numInfo = 0u;
-        RM_PROCESS_INFO[] processInfoArray;
+        Span<RM_PROCESS_INFO> processInfoArray;
         while (true) {
-            uint needed;
-            processInfoArray = new RM_PROCESS_INFO[numInfo];
-            fixed (RM_PROCESS_INFO* arrayPtr = processInfoArray) {
-                res = PInvoke.RmGetList(handle, out needed, ref numInfo, arrayPtr, out _);
-            }
+            processInfoArray = new Span<RM_PROCESS_INFO>(new RM_PROCESS_INFO[numInfo]);
+            res = PInvoke.RmGetList(handle, out uint needed, ref numInfo, processInfoArray, out _);
 
             if (res == WIN32_ERROR.NO_ERROR) {
                 break;
